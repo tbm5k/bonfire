a40bda99c4e2f1c1eeec574308a651b0b341a5e4
Teddy <tedburg5@gmail.com>
2021-08-23 16:27:00 +0300

    list css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,14 +4,16 @@ import './Header.css'
 function Header(){
     return(
         <div id="header">
-            <h4>Bonfire</h4>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>Places</li>
-                    <li>Deals</li>
-                </ul>
-            </nav>
+            <div className="header">
+                <h4>Bonfire</h4>
+                <nav>
+                    <ul>
+                        <li>Home</li>
+                        <li>Places</li>
+                        <li>Deals</li>
+                    </ul>
+                </nav>
+            </div>
         </div>
     );
 }
