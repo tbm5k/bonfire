bb7f5226a22486f22de0c626e4712ee3d6aa5511
tbm5k <tedburg5@gmail.com>
2021-08-24 17:52:11 +0300

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,36 @@ import './Footer.css'
 function Header(){
     return(
         <div id="footer">
-            Footer
+            <div className="footer">
+                <div id="handles">
+                    <h4>Social handles</h4>
+                </div>
+                <div id="about">
+                    <h4>About us</h4>
+                    <ul>
+                        <li>Offices</li>
+                        <li>Services</li>
+                        <li>Vision</li>
+                        <li>Mission</li>
+                        <li>Environmental mission</li>
+                        <li>Employee mission</li>
+                        <li>Client mission</li>
+                    </ul>
+                </div>
+                <div id="contacts">
+                    <h4>Contacts</h4>
+                    <ul>
+                        <li>+254-722-879-629</li>
+                        <li>+254-711-222-444</li>
+                        <li>+254-729-836-336</li>
+                        <li>+254-733-527-398</li>
+                        <li>+254-715-281-211</li>
+                        <li>0202067788</li>
+                        <li>deals@bonfireadventures.com</li>
+                        <li>info@bonfireadventures.com</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
