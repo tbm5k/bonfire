0bc860d7c06164cacccfdef232d1c8211f3f8e52
tbm5k <tedburg5@gmail.com>
2021-08-24 14:28:14 +0300

    footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+function Header(){
+    return(
+        <div id="footer">
+            Footer
+        </div>
+    );
+}
+
+export default Header;
