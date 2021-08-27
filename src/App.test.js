e07fd1242bb5d7f5c45825fd808fed137fdb72a3
Teddy <tedburg5@gmail.com>
2021-08-27 15:27:22 +0300

    wrapped the header test with browserrouter

--- src/App.test.js
+++ src/App.test.js
@@ -2,14 +2,19 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 test('renders app component', () => {
   render(<App />);
 });
 
 test('renders the header', () => {
-  render(<Header />);
-});
+  render(
+  <Router>
+    <Header />
+  </Router>
+    );
+  });
 
 test('rendres the footer', () => {
   render(<Footer />);
