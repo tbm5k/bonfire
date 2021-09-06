7f0e24df4827821516bb9879c5d1c0b5c5030957
Teddy <tedburg5@gmail.com>
2021-09-06 17:27:02 +0300

    get request to the continents list

--- src/context/ContinentContext.js
+++ src/context/ContinentContext.js
@@ -1,10 +1,22 @@
-import React, {createContext} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
+import axios from 'axios';
 
 export const ContinentContext = createContext();
 
 export const ContinentProvider = ({children}) => {
+    
+    const [continents, setContinents] = useState([]);
+
+    useEffect(() => {
+        async function getContinents(){
+            let data = await axios.get('localhost:8080/continents')
+            setContinents(data.data)
+        }
+        getContinents();
+    }, []);
+
     return(
-        <ContinentContext.Provider value={"Continents list"}>
+        <ContinentContext.Provider value={continents}>
             {children}
         </ContinentContext.Provider>
     );
