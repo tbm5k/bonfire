0676672886b821d0a421a6173d349e3d12fed730
tbm5k <tedburg5@gmail.com>
2021-09-15 15:57:32 +0300

    places page styled

--- src/pages/places/Places.js
+++ src/pages/places/Places.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import './Places.css';
+//import './Places.css';
 import Banner from '../../components/banner/Banner';
 import {ContinentContext} from '../../context/ContinentContext';
 import Cube from '../../components/cube/Cube';
@@ -10,14 +10,14 @@ export default function Places(){
 
     console.log(continents)
     return(
-        <div id="places">
+        <div>
             <Banner />
                 {
                     continents.map( continent => (
-                        <div id="continents">
-                            <h2>{continent.continentName}</h2>
+                        <div className='flex flex-col mt-9 mb-9'>
+                            <h2 className='text-center text-4xl font-bold'>{continent.continentName}</h2>
                             {console.log(continent.countryList)}
-                            <div id="countries">
+                            <div className='flex flex-row justify-evenly overflow-hidden mt-6 mb-6'>
                                 {
                                     continent.countryList.map( country => (
                                         <div>
@@ -26,6 +26,7 @@ export default function Places(){
                                     ))
                                 }
                             </div>
+                            <button className='bg-bonfireorange text-lg font-bold text-white rounded-xl w-52 h-11 mx-auto'>More</button>
                         </div>
                     ))
                 }
