4b7a67557b211924bf3b988e6c3221fa57f68d44
Teddy <tedburg5@gmail.com>
2021-10-30 12:18:43 +0300

    .

--- src/pages/countries/Countries.js
+++ src/pages/countries/Countries.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Cube from '../../components/cube/Cube';
+import { fetchCountries } from '../../redux/actions/countriesAction';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom'
 
 const Countries = () => {
+
+    const { continentId } = useParams();
+    console.log(continentId)
+
+    useEffect(() => {
+        fetchCountries();
+    }, []);
+
     return (
         <div className="flex flex-row justify-evenly">
-            <Cube />
-            <Cube />
             <Cube />
         </div>
     );
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { hotelReducer } from "./hotelReducer";
 import { continentReducer } from "./continentReducer";
 import { combineReducers } from "redux";
+import { countriesReducer } from "./countriesReducer";
 
 const reducers = combineReducers({
     continents: continentReducer,
-    hotels: hotelReducer
+    hotels: hotelReducer,
+    countries : countriesReducer
 })
 
 export default reducers;
