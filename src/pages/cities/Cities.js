d4df4a3ebdc170bffe2f19148d0dba716f281126
Teddy <tedburg5@gmail.com>
2021-11-02 13:49:34 +0300

    displaying the cities in a country

--- src/pages/cities/Cities.js
+++ src/pages/cities/Cities.js
@@ -1,11 +1,13 @@
 import React, { lazy, useEffect } from 'react'
 import { fetchCities } from '../../redux/actions/citiesActions';
-import { useDispatch } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux'; 
 
 const Tile = lazy(() => import('../../components/tile/Tile'))
 
 const Cities = ({match}) => {
 
+    const cities = useSelector( state => state.cities);
+    console.log(cities.cities.cityList)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -14,7 +16,15 @@ const Cities = ({match}) => {
 
     return(
         <div>
-            <Tile />
+            {
+                cities.cities.cityList !== undefined ? (
+                    cities.cities.cityList.map(city => {
+                        <Tile />
+                    })
+                ): (
+                    <div>No cities</div>
+                )
+            }
         </div>
     );
 };
