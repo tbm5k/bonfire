2162d590b2d593aaae37a92cee401177b1e0ca63
Teddy <tedburg5@gmail.com>
2021-11-09 22:47:25 +0300

    slideshow

--- src/components/slideshow/Slideshow.js
+++ src/components/slideshow/Slideshow.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import './Slideshow.css';
 import photo from '../images/slideshow/giraffe.jpg';
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css';
 
 function Slideshow(){
 
+    const images = [
+        {url: "https://images.freeimages.com/images/large-previews/0d6/blue-flowers-with-macro-4-1400913.jpg"},
+        {url: "https://images.freeimages.com/images/large-previews/60f/dubai-desert-safari-1407254.jpg"},
+    ];
+
     return(
         <div id="slideshow">
-            <img src={photo} alt="photos of scenaries"/>
+            <Slide>
+                {
+                    images.map( image => (
+                        <img src={image.url}/>
+                    ))
+                }
+            </Slide>
+            {/* <img src={photo} alt="photos of scenaries"/> */}
         </div>
     );
 }
